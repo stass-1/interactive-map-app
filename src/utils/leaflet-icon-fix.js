14a6cc16521622d99f97c8b35d6b0ab0ea6e7daa
Stan <x.stass@gmail.com>
2025-03-20 20:50:14 +0200

    Add src/utils/leaflet-icon-fix.js

---
+++ src/utils/leaflet-icon-fix.js
@@ -0,0 +1,18 @@
+// Этот файл исправляет проблему с отображением маркеров в Leaflet при использовании Webpack/Vite
+import L from 'leaflet'
+import iconUrl from 'leaflet/dist/images/marker-icon.png'
+import shadowUrl from 'leaflet/dist/images/marker-shadow.png'
+
+// Фикс для маркеров
+delete L.Icon.Default.prototype._getIconUrl
+L.Icon.Default.mergeOptions({
+	iconUrl,
+	shadowUrl,
+	iconSize: [25, 41],
+	iconAnchor: [12, 41],
+	popupAnchor: [1, -34],
+	tooltipAnchor: [16, -28],
+	shadowSize: [41, 41]
+})
+
+export default L
